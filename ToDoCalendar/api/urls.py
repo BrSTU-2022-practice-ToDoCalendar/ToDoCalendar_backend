190c0093c55166e54a09dd386d3eee472701d3dd
mugiwar <mugiwar8@gmail.com>
2022-06-16 22:13:09 +0300

    add some fix

--- ToDoCalendar/api/urls.py
+++ ToDoCalendar/api/urls.py
@@ -1,9 +1,9 @@
 from django.urls import include
 from django.urls import path
-from .views import RegisterViewSet
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt import views as jwt_views
 
+from .views import RegisterViewSet
 
 router = DefaultRouter()
 router.register(r'register', RegisterViewSet)
@@ -15,5 +15,6 @@ urlpatterns = [
          name='token_obtain_pair'),
     path('v1/login/refresh/', jwt_views.TokenRefreshView.as_view(),
          name='token_refresh'),
-    path('v1/login/verify/', jwt_views.TokenVerifyView.as_view()),
+    path('v1/login/verify/', jwt_views.TokenVerifyView.as_view(),
+         name='token_verify'),
 ]
--- ToDoCalendar/api/views.py
+++ ToDoCalendar/api/views.py
@@ -1,7 +1,6 @@
 from rest_framework import mixins
 from rest_framework import viewsets
 
-
 from .models import User
 from .serializers import RegisterSerializer
 
