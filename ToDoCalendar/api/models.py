21ed185bfde323095304c7981b4a5dfafc63660e
WolkNA <ruskajitgames@gmail.com>
2022-06-17 15:33:48 +0300

    Edit Task model

--- ToDoCalendar/api/models.py
+++ ToDoCalendar/api/models.py
@@ -7,7 +7,7 @@ User = get_user_model()
 
 class Task(models.Model):
     title = models.CharField('Title', max_length=255)
-    description = models.TextField('Description', blank=True)
+    description = models.TextField('Description', null=True)
     start_date = models.DateTimeField('Start date', auto_now_add=True)
     end_date = models.DateTimeField('End date', auto_now_add=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
