b343365ce8fd8871c3d7a4cd562408b9f1732fa2
WolkNA <ruskajitgames@gmail.com>
2022-06-22 20:52:03 +0300

    Remove dumb string

--- api/serializers.py
+++ api/serializers.py
@@ -1,4 +1,3 @@
-from asyncio.format_helpers import extract_stack
 from rest_framework import serializers
 from django.contrib.auth.models import User
 from django.contrib.auth.hashers import make_password
