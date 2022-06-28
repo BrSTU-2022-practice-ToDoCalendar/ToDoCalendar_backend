001ff87ed943c4eabdf4aa3311f5adb7e479a13c
mugiwar <mugiwar8@gmail.com>
2022-06-28 17:10:22 +0300

    some fix

---
+++ tests/test_login_user.py
@@ -0,0 +1,44 @@
+import pytest
+
+from django.urls import reverse
+from rest_framework import status
+
+
+@pytest.mark.django_db
+def test_correct_user_login(client, set_of_accounts_data):
+    url = reverse('token_obtain_pair')
+    data = {
+        'username': set_of_accounts_data['account1']['username'],
+        'password': set_of_accounts_data['account1']['password'],
+    }
+    response = client.post(url, data=data)
+    assert response.status_code == status.HTTP_200_OK
+    assert response.data['access'] is not None
+    assert response.data['refresh'] is not None
+
+
+@pytest.mark.django_db
+def test_invalid_user_login(client):
+    url = reverse('token_obtain_pair')
+    data = {
+        'username': '2testacc2',
+        'password': '1234',
+
+    }
+    response = client.post(url, data=data)
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+
+
+@pytest.mark.django_db
+def test_blank_user_login(client):
+    url = reverse('token_obtain_pair')
+    data = {
+        'username': ' ',
+        'password': ' ',
+
+    }
+    response = client.post(url, data=data)
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+    assert response.data['username'][0].code == 'blank'
+    assert response.data['password'][0].code == 'blank'
