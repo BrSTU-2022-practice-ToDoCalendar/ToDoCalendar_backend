e74119f877dc06033a4fff1fcf229186c6a92754
mugiwar <mugiwar8@gmail.com>
2022-06-28 17:16:11 +0300

    some fix

--- tests/test_login_user.py
+++ tests/test_login_user.py
@@ -8,13 +8,13 @@ from rest_framework import status
 def test_correct_user_login(client, set_of_accounts_data):
     url = reverse('token_obtain_pair')
     data = {
-        'username': 'testacc',
-        'password': '1234',
-
+        'username': set_of_accounts_data['account1']['username'],
+        'password': set_of_accounts_data['account1']['password'],
     }
     response = client.post(url, data=data)
     assert response.status_code == status.HTTP_200_OK
     assert response.data['access'] is not None
+    assert response.data['refresh'] is not None
 
 
 @pytest.mark.django_db
@@ -39,5 +39,5 @@ def test_blank_user_login(client):
     }
     response = client.post(url, data=data)
     assert response.status_code == status.HTTP_400_BAD_REQUEST
-    assert response.data['password'] is not None
-    assert response.data['username'] is not None
+    assert response.data['username'][0].code == 'blank'
+    assert response.data['password'][0].code == 'blank'
