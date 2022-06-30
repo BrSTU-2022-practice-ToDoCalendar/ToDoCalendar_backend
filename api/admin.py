908dfa393fca70c13266931163b37f7e0bd4aec6
Igor Lukovets <igor.lukavets@gmail.com>
2022-06-30 15:46:09 +0300

    edit admin panel

--- api/admin.py
+++ api/admin.py
@@ -2,18 +2,13 @@ from django.contrib import admin
 
 from .models import Task
 
+
 class TaskAdmin(admin.ModelAdmin):
+    list_editable = ('title', 'description', 'start_date', 'end_date', 'completed', 'user')
+    list_display = ('id', 'title', 'description', 'start_date', 'end_date', 'completed', 'user')
+    list_filter = ('title', 'description', 'user', 'completed')
+    search_fields = ('id', 'title', 'description', 'start_date', 'end_date', 'completed', 'user__username', 'user__id')
     empty_value_display = '-void-'
-    list_display = (
-        'id', 'title', 'description', 'start_date', 'end_date', 'completed', 
-        'user'
-    )
-    list_filter = ('title','description','user','completed')
-    readonly_fields = ('id',)
-    search_fields = (
-        'id', 'title', 'description', 'start_date', 'end_date', 'completed', 
-        'user__username'
-    )
     
 
 admin.site.register(Task, TaskAdmin)
