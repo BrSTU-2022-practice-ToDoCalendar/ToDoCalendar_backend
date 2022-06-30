f05187f8edd09a169a54b83fd1c23b946e147f2d
WolkNA <ruskajitgames@gmail.com>
2022-06-30 14:13:09 +0300

    Some fixes

--- api/admin.py
+++ api/admin.py
@@ -3,15 +3,17 @@ from django.contrib import admin
 from .models import Task
 
 class TaskAdmin(admin.ModelAdmin):
+    empty_value_display = '-void-'
     list_display = (
         'id', 'title', 'description', 'start_date', 'end_date', 'completed', 
         'user'
     )
     list_filter = ('title','description','user','completed')
+    readonly_fields = ('id',)
     search_fields = (
         'id', 'title', 'description', 'start_date', 'end_date', 'completed', 
         'user__username'
     )
+    
 
-admin.site.empty_value_display = '-void-'
 admin.site.register(Task, TaskAdmin)
