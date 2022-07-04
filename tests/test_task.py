f9a3866134f7a6438d3a2eb1212c745a60f4c062
Igor Lukovets <igor.lukavets@gmail.com>
2022-07-04 23:39:26 +0300

    add tests for task-list with params and for task-statuses

--- tests/test_task.py
+++ tests/test_task.py
@@ -408,6 +408,160 @@ class TestListTask:
         for resp in response.data:
             assert not second_user_tasks.filter(id=resp['id']).exists()
 
+    @pytest.mark.django_db
+    def test_list_task_with_year_param(
+            self,
+            client,
+            set_of_authenticated_accounts_data,
+    ):
+        url = reverse('task-list')
+
+        account = set_of_authenticated_accounts_data["authenticated_account1"]
+        user = User.objects.get(username=account['username'])
+
+        for i in range(10):
+            Task.objects.create(
+                title='task1',
+                description='string1',
+                start_date=f'201{i // 2}-08-24T14:15:22Z',
+                end_date='2019-10-24T14:15:22Z',
+                completed=False,
+                user=user
+            )
+
+        auth_header = (
+            'Bearer '
+            f'{account["access-token"]}'
+        )
+
+        response = client.get(
+            url,
+            {'year': 2013},
+            HTTP_AUTHORIZATION=auth_header,
+        )
+
+        assert response.status_code == status.HTTP_200_OK
+        assert len(response.data) == 2
+
+        for task in response.data:
+            assert Task.objects.get(id=task['id']).start_date.year == 2013
+
+    @pytest.mark.django_db
+    def test_list_task_with_month_param(
+            self,
+            client,
+            set_of_authenticated_accounts_data,
+    ):
+        url = reverse('task-list')
+
+        account = set_of_authenticated_accounts_data["authenticated_account1"]
+        user = User.objects.get(username=account['username'])
+
+        for i in range(2, 12):
+            Task.objects.create(
+                title='task1',
+                description='string1',
+                start_date=f'2019-0{i // 2}-24T14:15:22Z',
+                end_date='2019-10-24T14:15:22Z',
+                completed=False,
+                user=user
+            )
+
+        auth_header = (
+            'Bearer '
+            f'{account["access-token"]}'
+        )
+
+        response = client.get(
+            url,
+            {'month': 3},
+            HTTP_AUTHORIZATION=auth_header,
+        )
+
+        assert response.status_code == status.HTTP_200_OK
+        assert len(response.data) == 2
+
+        for task in response.data:
+            assert Task.objects.get(id=task['id']).start_date.month == 3
+
+    @pytest.mark.django_db
+    def test_list_task_with_day_param(
+            self,
+            client,
+            set_of_authenticated_accounts_data,
+    ):
+        url = reverse('task-list')
+
+        account = set_of_authenticated_accounts_data["authenticated_account1"]
+        user = User.objects.get(username=account['username'])
+
+        for i in range(2, 12):
+            Task.objects.create(
+                title='task1',
+                description='string1',
+                start_date=f'2019-08-0{i // 2}T14:15:22Z',
+                end_date='2019-10-24T14:15:22Z',
+                completed=False,
+                user=user
+            )
+
+        auth_header = (
+            'Bearer '
+            f'{account["access-token"]}'
+        )
+
+        response = client.get(
+            url,
+            {'day': 3},
+            HTTP_AUTHORIZATION=auth_header,
+        )
+
+        assert response.status_code == status.HTTP_200_OK
+        assert len(response.data) == 2
+
+        for task in response.data:
+            assert Task.objects.get(id=task['id']).start_date.day == 3
+
+    @pytest.mark.django_db
+    def test_list_task_with_all_params(
+            self,
+            client,
+            set_of_authenticated_accounts_data,
+    ):
+        url = reverse('task-list')
+
+        account = set_of_authenticated_accounts_data["authenticated_account1"]
+        user = User.objects.get(username=account['username'])
+
+        for i in range(2, 10):
+            Task.objects.create(
+                title='task1',
+                description='string1',
+                start_date=f'201{i // 2}-0{i}-0{i}T14:15:22Z',
+                end_date='2019-10-24T14:15:22Z',
+                completed=False,
+                user=user
+            )
+
+        auth_header = (
+            'Bearer '
+            f'{account["access-token"]}'
+        )
+
+        response = client.get(
+            url,
+            {'year': 2013, 'month': 6, 'day': 6},
+            HTTP_AUTHORIZATION=auth_header,
+        )
+
+        assert response.status_code == status.HTTP_200_OK
+        assert len(response.data) == 1
+
+        task = Task.objects.get(id=response.data[0]['id'])
+        assert task.start_date.year == 2013
+        assert task.start_date.month == 6
+        assert task.start_date.day == 6
+
 
 class TestUpdateTask:
 
@@ -1071,3 +1225,78 @@ class TestUpdateTask:
         assert end_date != data['end_date']
 
         assert task.user.username == acc['username']
+
+
+class TestStatusesTask:
+
+    @pytest.mark.django_db
+    def test_correct_statuses(
+            self,
+            client,
+            set_of_authenticated_accounts_data,
+    ):
+        url = reverse('task-statuses')
+
+        acc = set_of_authenticated_accounts_data['authenticated_account1']
+        auth_header = f'Bearer {acc["access-token"]}'
+
+        user = User.objects.get(username=acc['username'])
+
+        Task.objects.create(title='task1', description='string1',
+                            start_date='2019-10-01T14:15:22Z',
+                            end_date='2019-10-24T14:15:22Z',
+                            completed=False, user=user)
+        Task.objects.create(title='task1', description='string1',
+                            start_date='2019-10-01T14:15:22Z',
+                            end_date='2019-10-24T14:15:22Z',
+                            completed=False, user=user)
+
+        Task.objects.create(title='task1', description='string1',
+                            start_date='2019-10-02T14:15:22Z',
+                            end_date='2019-10-24T14:15:22Z',
+                            completed=False, user=user)
+        Task.objects.create(title='task1', description='string1',
+                            start_date='2019-10-02T14:15:22Z',
+                            end_date='2019-10-24T14:15:22Z',
+                            completed=True, user=user)
+
+        Task.objects.create(title='task1', description='string1',
+                            start_date='2019-10-03T14:15:22Z',
+                            end_date='2019-10-24T14:15:22Z',
+                            completed=True, user=user)
+        Task.objects.create(title='task1', description='string1',
+                            start_date='2019-10-03T14:15:22Z',
+                            end_date='2019-10-24T14:15:22Z',
+                            completed=True, user=user)
+
+        response = client.get(
+            url,
+            HTTP_AUTHORIZATION=auth_header,
+        )
+
+        assert response.status_code == status.HTTP_200_OK
+        assert len(response.data) == 3
+
+        assert response.data[0]['date'] == '2019-10-01T00:00:00Z'
+        assert not response.data[0]['completed']
+        assert response.data[0]['not_completed']
+
+        assert response.data[1]['date'] == '2019-10-02T00:00:00Z'
+        assert response.data[1]['completed']
+        assert response.data[1]['not_completed']
+
+        assert response.data[2]['date'] == '2019-10-03T00:00:00Z'
+        assert response.data[2]['completed']
+        assert not response.data[2]['not_completed']
+
+    @pytest.mark.django_db
+    def test_list_task_by_unauthorized_user(
+            self,
+            client,
+    ):
+        url = reverse('task-statuses')
+
+        response = client.get(url)
+
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
+        assert response.data['detail'].code == 'not_authenticated'
