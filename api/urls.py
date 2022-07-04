75533b87c619cc8dc176deffa958daaf9d7a645b
Igor Lukovets <igor.lukavets@gmail.com>
2022-07-05 00:04:11 +0300

    move redoc and swagger to api/v1/

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,8 @@
-from django.urls import include
-from django.urls import path
+from django.urls import include, path, re_path
+from rest_framework import permissions
 from rest_framework.routers import DefaultRouter
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 from .views import (
     RegisterViewSet,
@@ -11,13 +13,35 @@ from .views import (
 )
 
 
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Snippets API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
+
+
 router = DefaultRouter()
 router.register(r'register', RegisterViewSet, basename='register')
 router.register(r'tasks', TaskViewSet, basename='task')
 
 
 urlpatterns = [
+    re_path(r'v1/^swagger(?P<format>\.json|\.yaml)$',
+            schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('v1/swagger/', schema_view.with_ui('swagger', cache_timeout=0),
+         name='schema-swagger-ui'),
+    path('v1/redoc/', schema_view.with_ui('redoc', cache_timeout=0),
+         name='schema-redoc'),
+
     path('v1/', include(router.urls)),
+
     path('v1/login/', DecoratedToSwaggerTokenObtainPairView.as_view(),
          name='token_obtain_pair'),
     path('v1/refresh-token/', DecoratedToSwaggerTokenRefreshView.as_view(),
